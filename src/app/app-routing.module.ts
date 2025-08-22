bf550887fe3185166c1b56fcf81628a277ba1d50
danglephd@gmail.com <danglephd@gmail.com>
2025-08-22 15:11:45 +0700

    Remove unused code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IssueComponent } from './issue/issue.component';
 import { WalletComponent } from './wallet/wallet.component';
-import { WalletAddDialogComponent } from './wallet-add-dialog/wallet-add-dialog.component';
-import { CalendarDialogComponent } from './calendar-dialog/calendar-dialog.component';
-import { BillDetailComponent } from './bill-detail/bill-detail.component';
 
 const routes: Routes = [
  { path: '', redirectTo: 'issue', pathMatch: 'full' },
  { path: 'issue', component: IssueComponent },
- { path: 'wallet', component: WalletComponent },
- { path: 'walletDialog', component: WalletAddDialogComponent },
- { path: 'calendarDialog', component: CalendarDialogComponent },
- { path: 'billDetailDialog', component: BillDetailComponent },
+ { path: 'wallet', component: WalletComponent }
 ]; 
 
 @NgModule({
